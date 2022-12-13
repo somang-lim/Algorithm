d997da508eff2d1cda14b82971566f8e035142d3
imhope <105486505+somang-lim@users.noreply.github.com>
2022-12-13 21:26:10 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.35 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        Arrays.sort(array);
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > height) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
