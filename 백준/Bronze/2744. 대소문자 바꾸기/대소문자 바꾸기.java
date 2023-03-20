7b29cea9ddd77683e48a9fffcf0b24572fad43c6
imhope <105486505+somang-lim@users.noreply.github.com>
2023-03-21 01:56:11 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 208 ms, Memory: 17544 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -4,18 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
+        char[] answer = str.toCharArray();
 
 		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-
-			// 1. 대문자/소문자 구별
-			if ('A' <= ch && ch <= 'Z') {
-                // 2. 대문자 --> 소문자
-				System.out.print((char) ('a' + ch - 'A'));
-			} else {
-                // 2. 소문자 --> 대문자
-				System.out.print((char) ('A' + ch - 'a'));
-            }
+            if ('a' <= answer[i] && answer[i] <= 'z')
+                answer[i] = (char) ('A' + answer[i] - 'a');
+            else 
+                answer[i] = (char) ('a' + answer[i] - 'A');
 		}
+        
+        System.out.println(answer);
 	}
 }
