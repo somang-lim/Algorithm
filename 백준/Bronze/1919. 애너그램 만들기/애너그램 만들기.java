771697c55ce75fd51f604868d8e0c47d0211ff7e
imhope <105486505+somang-lim@users.noreply.github.com>
2023-03-21 02:21:50 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 200 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,29 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    public static int[] get_alphabet_count(String str) {
+        int[] count = new int[26];
+        
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'a']++;
+        }
+        
+        return count;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
         
-        int[] a_count = new int[26];
-        int[] b_count = new int[26];
-        
-        for (int i = 0; i < a.length(); i++) {
-            a_count[a.charAt(i) - 'a']++;
-        }
-        
-        for (int i = 0; i < b.length(); i++) {
-            b_count[b.charAt(i) - 'a']++;
-        }
+        int[] a_count = get_alphabet_count(a);
+        int[] b_count = get_alphabet_count(b);
         
         int ans = 0;
         for (int i = 0; i < 26; i++) {
-            if (a_count[i] > b_count[i]) {
-                ans += a_count[i] - b_count[i];
-            } else if (a_count[i] < b_count[i]) {
-                ans += b_count[i] - a_count[i];
-            }
+            ans += Math.abs(a_count[i] - b_count[i]);
         }
         
         System.out.println(ans);
