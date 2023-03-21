e0919f41460e3cc2517420494557d99658ba460c
imhope <105486505+somang-lim@users.noreply.github.com>
2023-03-21 13:23:26 +0900

    [Bronze I] Title: 단어 공부, Time: 360 ms, Memory: 28388 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,36 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-	public static int[] getAlphabetCount(String str) {
-		int[] count = new int[26];
-
-		for (int i = 0; i < str.length(); i++) {
-			count[str.charAt(i) - 'A']++;
-		}
-
-		return count;
-	}
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next().toUpperCase();
-
-		// 1. 알파벳 개수 세기
-		int[] count = getAlphabetCount(str);
-
-		// 2. 가장 많이 사용된 알파벳 도출하기
-		int max_count = -1;
-		char max_alphabet = '?';
-		for (int i = 0; i < 26; i++) {
-			if (count[i] > max_count) {
-				max_count = count[i];
-				max_alphabet = (char)('A' + i);
-			}
-            else if (count[i] == max_count) {
-				max_alphabet = '?';
+		Scanner in = new Scanner(System.in);
+		
+		int[] arr = new int[26];
+		String R = in.next().toUpperCase();
+		
+		for(int i = 0; i < R.length(); i++) {
+			int num = R.charAt(i) - 'A';
+			arr[num]++;
+		}
+		
+		int max = 0;
+		char answer = '?';
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+				answer = (char)(i + 'A');
+			} else if(max == arr[i]) {
+				answer = '?';
 			}
 		}
-
-		System.out.println(max_alphabet);
+		
+		System.out.println(answer);
+		
+		in.close();
 	}
+	
 }
