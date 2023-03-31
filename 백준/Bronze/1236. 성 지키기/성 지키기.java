c135194e4960830f7c32a1d3495ee231f9fd6f08
imhope <105486505+somang-lim@users.noreply.github.com>
2023-03-31 16:45:15 +0900

    [Bronze I] Title: 성 지키기, Time: 80 ms, Memory: 11788 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int col_sum = 0;
+		int row_sum = 0;
+
+		boolean[][] castle = new boolean[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				if (str.charAt(j) == '.') castle[i][j] = false;
+				if (str.charAt(j) == 'X') castle[i][j] = true;
+			}
+		}
+
+		// 열 검사
+		for (int i = 0; i < M; i++) {
+			int count = 0;
+
+			for (int j = 0; j < N; j++) {
+				if (castle[j][i] == false) count++;
+			}
+
+			if (count == N) col_sum++; // 열에 필요한 경비원 수
+		}
+
+		for (int i = 0; i < N; i++) {
+			int count = 0;
+
+			for (int j = 0; j < M; j++) {
+				if (castle[i][j] == false) count++;
+			}
+
+			if (count == M) row_sum++; // 행에 필요한 경비원 수
+		}
+
+		System.out.println(row_sum >= col_sum ? row_sum : col_sum);
+	}
+
+}
