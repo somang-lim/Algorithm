f46a936be842b215f1732fb891a80c88089d749c
imhope <105486505+somang-lim@users.noreply.github.com>
2023-03-31 15:01:24 +0900

    [Silver IV] Title: 개미, Time: 112 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -5,33 +5,34 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int W = sc.nextInt();
-		int H = sc.nextInt();
-		int P = sc.nextInt();
-		int Q = sc.nextInt();
-		int T = sc.nextInt();
-
-		int timeX = T % (2 * W);
-		int currentX = P;
-		int deltaX = 1;
-
-		while (timeX-- > 0) {
-			if (currentX == W) deltaX = -1;
-			else if (currentX == 0) deltaX = 1;
-			currentX += deltaX;
+		int W = sc.nextInt(); // 열
+		int H = sc.nextInt(); // 행
+		int P = sc.nextInt(); // 열 좌표
+		int Q = sc.nextInt(); // 행 좌표
+		int T = sc.nextInt(); // 시간
+		
+		int xt = T % (2 * W); // 열
+		boolean flag = false;
+		
+		for (int i = 0; i < xt; i++) {
+			// flag 가 true 인 경우에는 1씩 감소, false 인 경우에는 1씩 증가 -> 벽에 부딪치는 상황 구현
+			if (!flag) P++;
+			else P--;
+			if (P == W) flag = true;
+			else if (P == 0) flag = false;
+		}
+		
+		int yt = T % (2 * H); // 행
+		flag = false;
+		
+		for (int i = 0; i < yt; i++) {
+			if (!flag) Q++;
+			else Q--;
+			if (Q == H) flag = true;
+			else if (Q == 0) flag = false;
 		}
 
-		int timeY = T % (2 * H);
-		int currentY = Q;
-		int deltaY = 1;
-
-		while (timeY-- > 0) {
-			if (currentY == H) deltaY = -1;
-			else if (currentY == 0) deltaY = 1;
-			currentY += deltaY;
-		}
-
-		System.out.println(currentX + " " + currentY);
+		System.out.println(P + " " + Q);
 	}
 
 }
