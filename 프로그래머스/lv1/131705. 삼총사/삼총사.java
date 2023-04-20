5ea433540fd5f4afd67bf8d6fc36ec64c08c77f5
imhope <105486505+somang-lim@users.noreply.github.com>
2023-04-20 16:48:39 +0900

    [level 1] Title: 삼총사, Time: 0.32 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        Arrays.sort(number);
+        
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
