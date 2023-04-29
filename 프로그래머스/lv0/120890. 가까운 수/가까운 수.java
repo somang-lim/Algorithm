dc0b4ab88c3f05bcb11a59ffdab5a1203db3665c
imhope <105486505+somang-lim@users.noreply.github.com>
2023-04-29 20:46:35 +0900

    [level 0] Title: 가까운 수, Time: 0.33 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        Arrays.sort(array);
+        
+        for (int i = 0; i < array.length; i++) {
+            if (Math.abs(n - array[0]) > Math.abs(n - array[i])) {
+                array[0] = array[i];
+            }
+        }
+        
+        answer = array[0];
+        
+        return answer;
+    }
+}
